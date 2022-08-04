93f8eaafc1d0924e7f7a5310af22936e12b7dbfd
Amr Keleg <amr_mohamed@live.com>
2022-08-04 10:55:33 +0100

    Update the list of Arabic models

--- mlama/eval/groups.py
+++ mlama/eval/groups.py
@@ -1,7 +1,6 @@
 models_groups = {
     "Arabic_DA": ["darijabert", "dziribert", "camel_da", "marbert",],
-    "Arabic_MSA": ["arabert_base_v2", "arabic_bert", "arbert", "camel_msa",],
-    # "Arabic_MSA": ["arabert_base_v2", "arabert_large_v2", "arabic_bert", "arbert", "camel_msa",],
+    "Arabic_MSA": ["arabert_base_v2", "arabert_large_v2", "arabic_bert", "arbert", "camel_msa",],
     "Arabic_Mixed": ["camel_mix", "qarib",],
     "Arabic_Crosslingual": ["mbert_base_ar", "gigabert_v4_ar",],
     "English": ["bert-base"],
