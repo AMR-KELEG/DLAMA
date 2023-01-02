95826c95ae3a0a9ecd73a20699d5802a0b404e6c
Amr Keleg <amr_mohamed@live.com>
2023-01-02 12:34:16 +0000

    Avoid hardcoding the special token values

--- mlama/scripts/modules/base_connector.py
+++ mlama/scripts/modules/base_connector.py
@@ -4,71 +4,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
-import re
 import torch
-
-MASK = "[MASK]"
-BERT_UNK = "[UNK]"
-BERT_CLS = "[CLS]"
-BERT_SEP = "[SEP]"
-BERT_PAD = "[PAD]"
-ELMO_UNK = "<UNK>"
-ELMO_START_SENTENCE = "<S>"
-ELMO_END_SENTENCE = "</S>"
-OPENAI_UNK = "<unk>"
-OPENAI_EOS = "<eos>"
-ROBERTA_MASK = "<mask>"
-ROBERTA_PAD = "<pad>"
-ROBERTA_CLS = "<s>"
-ROBERTA_UNK = "<unk>"
-ROBERTA_START_SENTENCE = "<s>"
-ROBERTA_END_SENTENCE = "</s>"
-ROBERTA_VOCAB_SIZE = 50266
-
-SPECIAL_SYMBOLS = [
-    MASK,
-    BERT_UNK,
-    BERT_CLS,
-    BERT_SEP,
-    BERT_PAD,
-    ELMO_UNK,
-    ELMO_START_SENTENCE,
-    ELMO_END_SENTENCE,
-    OPENAI_UNK,
-    OPENAI_EOS,
-]
-
-SPACE_NORMALIZER = re.compile(r"\s+")
-
-
-def default_tokenizer(line):
-    """Default tokenizer for models that don't have one
-
-    Args:
-        line: a string representing a sentence
-
-    Returns:
-        A list of tokens
-    """
-
-    line = SPACE_NORMALIZER.sub(" ", line)
-    line = line.strip()
-    line = line.replace(
-        MASK, " " + str(MASK) + " "
-    )  # make sure MASK is correctly splitted
-
-    # fix tokenization for parentheses
-    line = line.replace("(", " ( ")
-    line = line.replace(")", " ) ")
-
-    # fix tokenization for comma
-    line = line.replace(",", " , ")
-
-    # fix tokenization for -- (e.g., 1954--1988)
-    line = line.replace("--", " -- ")
-
-    result = line.split()
-    return result
 
 
 class Base_Connector:
@@ -130,25 +66,4 @@ class Base_Connector:
         return log_probs, token_ids, masked_indices
 
     def get_batch_generation(self, sentences_list, logger=None, try_cuda=True):
-        raise NotImplementedError()
-
-    def get_contextual_embeddings(self, sentences):
-        """Compute the contextual embeddings of a list of sentences
-
-        Parameters:
-        sentences (list[list[string]]): list of elements. Each element is a list
-                                        that contains either a single sentence
-                                        or two sentences
-
-        Returns:
-        encoder_layers (list(Tensor)): a list of the full sequences of encoded-hidden-states
-                            at the end of each attention block (e.g., 12 full
-                            sequences for BERT-base,), each encoded-hidden-state
-                            is a torch.FloatTensor of size [batch_size,
-                            sequence_length, hidden_size]
-        sentence_lengths (list[int]): list of lenghts for the sentences in the
-                                      batch
-        tokenized_text_list: (list[list[string]]): tokenized text for the sentences
-                                                   in the batch
-        """
         raise NotImplementedError()
